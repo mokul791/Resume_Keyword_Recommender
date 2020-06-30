0afda630bf5278007aef1f64224b5e54d2855717
IsmailMokul <ismailmokul@gmail.com>
2020-06-30 14:20:05 -0400

    1st

---
+++ wordFreqCounter.py
@@ -0,0 +1,16 @@
+import nltk
+import pdfReader as pdf
+
+file = 'IBM Business Consultant.pdf'
+file_read = pdf.getTextPDF(file)
+print(file_read)
+
+# 	
+# file = 'IBM Business Consultant.txt'
+# for word in file_read:
+# 	print(word)
+# 
+fdist = nltk.FreqDist(pdf.getTextPDF(file))
+print("the number of distinct words in the bag: ", fdist.N())
+print(fdist[fdist.max()])
+print(fdist.most_common())
